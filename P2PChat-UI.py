5988dbdcd7c0205c68d9673092a0723e5f48e312
Who are you <beautiful@Whos-MacBook-Pro.local>
2019-03-25 21:45:54 +0800

    init docs

--- P2PChat-UI.py
+++ P2PChat-UI.py
@@ -11,10 +11,10 @@ from tkinter import *
 import sys
 import socket
 
+
 #
 # Global variables
 #
-
 
 
 #
@@ -22,15 +22,15 @@ import socket
 # Hash ID for each peer.
 # Source: http://www.cse.yorku.ca/~oz/hash.html
 #
-# Concatenate the peer's username, str(IP address), 
-# and str(Port) to form a string that be the input 
+# Concatenate the peer's username, str(IP address),
+# and str(Port) to form a string that be the input
 # to this hash function
 #
 def sdbm_hash(instr):
-	hash = 0
-	for c in instr:
-		hash = int(ord(c)) + (hash << 6) + (hash << 16) - hash
-	return hash & 0xffffffffffffffff
+    hash = 0
+    for c in instr:
+        hash = int(ord(c)) + (hash << 6) + (hash << 16) - hash
+    return hash & 0xffffffffffffffff
 
 
 #
@@ -38,30 +38,35 @@ def sdbm_hash(instr):
 #
 
 def do_User():
-	outstr = "\n[User] username: "+userentry.get()
-	CmdWin.insert(1.0, outstr)
-	userentry.delete(0, END)
+    username = userentry.get()
+    username = [c for c in username if c != ':'][:32]
+    username = ''.join(username)
+
+    outstr = "\n[User] username: " + username
+    CmdWin.insert(1.0, outstr)
+    userentry.delete(0, END)
 
 
 def do_List():
-	CmdWin.insert(1.0, "\nPress List")
+    CmdWin.insert(1.0, "\nPress List")
+    MsgWin.insert(1.0, "\n[Message Window] List is pressed")
 
 
 def do_Join():
-	CmdWin.insert(1.0, "\nPress JOIN")
+    CmdWin.insert(1.0, "\nPress JOIN")
 
 
 def do_Send():
-	CmdWin.insert(1.0, "\nPress Send")
+    CmdWin.insert(1.0, "\nPress Send")
 
 
 def do_Poke():
-	CmdWin.insert(1.0, "\nPress Poke")
+    CmdWin.insert(1.0, "\nPress Poke")
 
 
 def do_Quit():
-	CmdWin.insert(1.0, "\nPress Quit")
-	sys.exit(0)
+    CmdWin.insert(1.0, "\nPress Quit")
+    sys.exit(0)
 
 
 #
@@ -70,55 +75,64 @@ def do_Quit():
 win = Tk()
 win.title("MyP2PChat")
 
-#Top Frame for Message display
+# Top Frame for Message display
 topframe = Frame(win, relief=RAISED, borderwidth=1)
 topframe.pack(fill=BOTH, expand=True)
 topscroll = Scrollbar(topframe)
-MsgWin = Text(topframe, height='15', padx=5, pady=5, fg="red", exportselection=0, insertofftime=0)
+MsgWin = Text(topframe, height='15', padx=5, pady=5,
+              fg="red", exportselection=0, insertofftime=0)
 MsgWin.pack(side=LEFT, fill=BOTH, expand=True)
 topscroll.pack(side=RIGHT, fill=Y, expand=True)
 MsgWin.config(yscrollcommand=topscroll.set)
 topscroll.config(command=MsgWin.yview)
 
-#Top Middle Frame for buttons
+# Top Middle Frame for buttons
 topmidframe = Frame(win, relief=RAISED, borderwidth=1)
 topmidframe.pack(fill=X, expand=True)
-Butt01 = Button(topmidframe, width='6', relief=RAISED, text="User", command=do_User)
-Butt01.pack(side=LEFT, padx=8, pady=8);
-Butt02 = Button(topmidframe, width='6', relief=RAISED, text="List", command=do_List)
-Butt02.pack(side=LEFT, padx=8, pady=8);
-Butt03 = Button(topmidframe, width='6', relief=RAISED, text="Join", command=do_Join)
-Butt03.pack(side=LEFT, padx=8, pady=8);
-Butt04 = Button(topmidframe, width='6', relief=RAISED, text="Send", command=do_Send)
-Butt04.pack(side=LEFT, padx=8, pady=8);
-Butt06 = Button(topmidframe, width='6', relief=RAISED, text="Poke", command=do_Poke)
-Butt06.pack(side=LEFT, padx=8, pady=8);
-Butt05 = Button(topmidframe, width='6', relief=RAISED, text="Quit", command=do_Quit)
-Butt05.pack(side=LEFT, padx=8, pady=8);
+Butt01 = Button(topmidframe, width='6', relief=RAISED,
+                text="User", command=do_User)
+Butt01.pack(side=LEFT, padx=8, pady=8)
+Butt02 = Button(topmidframe, width='6', relief=RAISED,
+                text="List", command=do_List)
+Butt02.pack(side=LEFT, padx=8, pady=8)
+Butt03 = Button(topmidframe, width='6', relief=RAISED,
+                text="Join", command=do_Join)
+Butt03.pack(side=LEFT, padx=8, pady=8)
+Butt04 = Button(topmidframe, width='6', relief=RAISED,
+                text="Send", command=do_Send)
+Butt04.pack(side=LEFT, padx=8, pady=8)
+Butt06 = Button(topmidframe, width='6', relief=RAISED,
+                text="Poke", command=do_Poke)
+Butt06.pack(side=LEFT, padx=8, pady=8)
+Butt05 = Button(topmidframe, width='6', relief=RAISED,
+                text="Quit", command=do_Quit)
+Butt05.pack(side=LEFT, padx=8, pady=8)
 
-#Lower Middle Frame for User input
+# Lower Middle Frame for User input
 lowmidframe = Frame(win, relief=RAISED, borderwidth=1)
 lowmidframe.pack(fill=X, expand=True)
 userentry = Entry(lowmidframe, fg="blue")
 userentry.pack(fill=X, padx=4, pady=4, expand=True)
 
-#Bottom Frame for displaying action info
+# Bottom Frame for displaying action info
 bottframe = Frame(win, relief=RAISED, borderwidth=1)
 bottframe.pack(fill=BOTH, expand=True)
 bottscroll = Scrollbar(bottframe)
-CmdWin = Text(bottframe, height='15', padx=5, pady=5, exportselection=0, insertofftime=0)
+CmdWin = Text(bottframe, height='15', padx=5, pady=5,
+              exportselection=0, insertofftime=0)
 CmdWin.pack(side=LEFT, fill=BOTH, expand=True)
 bottscroll.pack(side=RIGHT, fill=Y, expand=True)
 CmdWin.config(yscrollcommand=bottscroll.set)
 bottscroll.config(command=CmdWin.yview)
 
-def main():
-	if len(sys.argv) != 4:
-		print("P2PChat.py <server address> <server port no.> <my port no.>")
-		sys.exit(2)
 
-	win.mainloop()
+def main():
+    if len(sys.argv) != 4:
+        print("P2PChat.py <server address> <server port no.> <my port no.>")
+        sys.exit(2)
+
+    win.mainloop()
+
 
 if __name__ == "__main__":
-	main()
-
+    main()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # chatroom
 ## Stage One (by Mar 26)
 Stage one includes the following functions:
-- do_User()
+- do_User() `Zhijing`
 - do_List()
 - part of the do_Join() function, which involves the interactions between the P2PChat program and
 the Room server in handling the JOIN request and the subsequent JOIN requests in the KEEPALIVE procedure. To show that the program has successfully joined the chatroom, it simply displays the members’ info to the Command Window.
